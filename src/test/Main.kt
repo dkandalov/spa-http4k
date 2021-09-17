9e003612266295d863de4717ed00e28fbbe6a684
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-09-17 09:55:05 +0100

    basic routing by Raul

--- src/test/Main.kt
+++ src/test/Main.kt
@@ -2,15 +2,25 @@ import org.http4k.client.OkHttp
 import org.http4k.core.*
 import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.ApacheServer
 import org.http4k.server.asServer
 
-fun main() {
-    val httpServer: HttpHandler = { request: Request ->
+val routes = routes(
+    "/hello" bind GET to {
         Response(OK).body("Hello 🌍")
-    }
+    },
+    "/bye" bind GET to { request ->
+        val name = request.query("name")
+        Response(OK).body("Bye, $name 👋")
+    },
+)
+
+fun main() {
+    val httpServer: HttpHandler = routes
     httpServer.asServer(ApacheServer(port = 8080)).start()
 
     val httpClient = OkHttp()
-    println(httpClient(Request(GET, "http://localhost:8080")))
+    println(httpClient(Request(GET, "http://localhost:8080/bye?name=Raul")))
 }
--- src/test/Tests.kt
+++ src/test/Tests.kt
@@ -4,17 +4,15 @@ import com.natpryce.hamkrest.equalTo
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
 
 class BackendTests {
     @Test fun `hello world`() {
-        val httpHandler: HttpHandler = {
-            Response(OK).body("Hello 🌍")
-        }
 
-        val response = httpHandler(Request(GET, "/"))
+        val httpHandler: HttpHandler = routes
+
+        val response = httpHandler(Request(GET, "/hello"))
 
         assertThat(response.status, equalTo(OK))
     }
