3e9adc5a6a789691c62c6ff818cef25f28d050ec
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-09-17 09:37:25 +0100

    hello world

--- src/test/Main.kt
+++ src/test/Main.kt
@@ -1,3 +1,16 @@
+import org.http4k.client.OkHttp
+import org.http4k.core.*
+import org.http4k.core.Method.GET
+import org.http4k.core.Status.Companion.OK
+import org.http4k.server.ApacheServer
+import org.http4k.server.asServer
+
 fun main() {
-    println("Hello 🌍")
+    val httpServer: HttpHandler = { request: Request ->
+        Response(OK).body("Hello 🌍")
+    }
+    httpServer.asServer(ApacheServer(port = 8080)).start()
+
+    val httpClient = OkHttp()
+    println(httpClient(Request(GET, "http://localhost:8080")))
 }
